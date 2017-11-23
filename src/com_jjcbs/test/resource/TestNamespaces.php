0fdcb3ed691411294b8f860f8466b8d6ece5145b
ndj888 <jjctc888@gmail.com>
2017-11-23 11:20:48 +0800

    修复引用循环造成的别名不解析bug

---
+++ src/com_jjcbs/test/resource/TestNamespaces.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JiangJiaCai.
+ * User: Administrator
+ * Date: 2017/11/23 0023
+ * Time: 11:07
+ */
+
+namespace com_jjcbs\test\resource;
+//{use template}
+
+/**
+ * @@Service()
+ * Class TestNamespaces
+ * @package com_jjcbs\test\resource
+ */
+class TestNamespaces
+{
+    //{method template}
+
+    /**
+     * @@Autowired(type="com_jjcbs\\service\\AnnotationServiceImpl")
+     * @var null
+     */
+    private $service = null;
+    public function __construct()
+    {
+//{construct template}
+    }
+}
